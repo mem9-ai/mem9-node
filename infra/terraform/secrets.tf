f0e18dd78dfccfce6160f1cbd2088f93e81c95eb
Bosn <bosn@outlook.com>
2026-03-28 10:37:43 +0800

    Format terraform secret mapping

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -6,10 +6,10 @@ resource "aws_secretsmanager_secret_version" "app" {
   secret_id = aws_secretsmanager_secret.app.id
 
   secret_string = jsonencode({
-    DATABASE_URL              = "mysql://${var.database_username}:${var.database_password}@${aws_db_instance.mysql.address}:3306/${var.database_name}"
-    APP_PEPPER                = var.app_pepper
-    GO_INTERNAL_SHARED_SECRET = var.go_internal_shared_secret
-    QWEN_API_KEY              = var.qwen_api_key
+    DATABASE_URL                                  = "mysql://${var.database_username}:${var.database_password}@${aws_db_instance.mysql.address}:3306/${var.database_name}"
+    APP_PEPPER                                    = var.app_pepper
+    GO_INTERNAL_SHARED_SECRET                     = var.go_internal_shared_secret
+    QWEN_API_KEY                                  = var.qwen_api_key
     DEEP_ANALYSIS_DAILY_LIMIT_BYPASS_FINGERPRINTS = var.deep_analysis_daily_limit_bypass_fingerprints
   })
 }
